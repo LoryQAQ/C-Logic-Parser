cfbaa42a3e8f1524e7f3cdc036c9aa71f78ea499
Henry Zhang <henryzhang9802@gmail.com>
2019-11-03 16:53:12 +0000

    Init

---
+++ logic-parser.c
@@ -0,0 +1,63 @@
+#include <stdio.h> 
+#include <string.h>   /* for all the new-fangled string functions */
+#include <stdlib.h>     /* malloc, free, rand */
+
+int Fsize=50; /* max string length of formulas*/
+int inputs=10;
+
+
+int i;
+int j;
+
+
+/*put all your functions here.  You will need
+1.
+int parse(char *g) which returns 1 if a proposition, 2 if neg, 3 if binary, ow 0
+Of course you will almost certainly need other functions.
+
+For binary formulas you will also need functions that return the first part and the second part of the binary formula.
+
+char *partone(char *g)
+
+char *parttwo(char *g)
+
+
+You may vary this program provided it reads 10 formulas in a file called "input.txt" and outputs in the way indicated below to a file called "output.txt".
+*/
+
+int main()
+{ /*input 10 strings from "input.txt" */
+  char *names[inputs];/*to store each of the input strings*/
+
+  for (i=0;i<inputs;i++) names[i]=malloc(Fsize);/*create enough space*/
+
+
+
+  FILE *fp, *fpout, *fopen();
+
+  if ((  fp=fopen("input.txt","r"))==NULL){printf("Error opening file");exit(1);}
+  if ((  fpout=fopen("output.txt","w"))==NULL){printf("Error opening file");exit(1);}/*ouputs to be sent to "output.txt"*/
+
+  fscanf(fp,"%s %s %s %s %s %s",names[0],names[1], names[2], names[3],names[4],names[5],names[6],names[7],names[8],names[9]);/*read input strings from "input.txt"*/
+ 
+  /*lets check your parser*/
+  for(i=0;i<inputs;i++)
+    {j=parse(names[i]);
+      switch(j)
+	{
+	case(0):fprintf(fpout,"%s is not a formula\n", names[i]);break;
+	case(1):fprintf(fpout,"%s is a proposition\n",names[i]);break;
+	case(2):fprintf(fpout,"%s is a negation\n",names[i]);break;
+	case(3):fprintf(fpout,"%s is a binary formula, the first part is %s and the second part is %s\n",names[i]), partone(names[i]), parttwo(names[i]);break;
+	default:fprintf(fpout,"%s is not a formula\n",names[i]);break;
+	}
+    }
+ 
+ 
+  fclose(fp);
+  fclose(fpout);
+ 
+  return(0);
+}
+1
+Downloading1

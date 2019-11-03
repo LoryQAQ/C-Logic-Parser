0dd78f8279647f5d4d37b8326f6a072be62c1649
Henry Zhang <henryzhang9802@gmail.com>
2019-11-03 19:27:57 +0000

    Add basic matcher

--- logic-parser.c
+++ logic-parser.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <ctype.h>
 #include <string.h>   /* for all the new-fangled string functions */
 #include <stdlib.h>     /* malloc, free, rand */
 
@@ -25,6 +26,48 @@ char *parttwo(char *g)
 You may vary this program provided it reads 10 formulas in a file called "input.txt" and outputs in the way indicated below to a file called "output.txt".
 */
 
+
+
+const char * matcher(char c){
+
+  if(c == 'p' || c == 'q' || c == 'r'){
+    return "PR";
+  }else if(c == 'v' || c == '^' || c == '>'){
+    return "BC";
+  }else if(c == '('){
+    return "LB";
+  }else if(c == ')'){
+    return "RB";
+  }else{
+    return "ER";
+  }
+
+}
+
+int parse(char *g) {
+  
+  for(int i = 0; g[i] != '\0'; i++) {
+      printf("%s ",matcher(g[i]));
+
+  }
+  printf("\n");
+  
+
+  return 0;
+}
+
+
+char *partone(char *g){
+  
+}
+
+char *parttwo(char *g){
+
+}
+
+
+
+
 int main()
 { /*input 10 strings from "input.txt" */
   char *names[inputs];/*to store each of the input strings*/
@@ -35,22 +78,22 @@ int main()
 
   FILE *fp, *fpout, *fopen();
 
-  if ((  fp=fopen("input.txt","r"))==NULL){printf("Error opening file");exit(1);}
-  if ((  fpout=fopen("output.txt","w"))==NULL){printf("Error opening file");exit(1);}/*ouputs to be sent to "output.txt"*/
+  if ((  fp=fopen("input.txt","r"))==NULL){ printf("Error opening file");exit(1); }
+  if ((  fpout=fopen("output.txt","w"))==NULL){ printf("Error opening file");exit(1); }/*ouputs to be sent to "output.txt"*/
 
   fscanf(fp,"%s %s %s %s %s %s",names[0],names[1], names[2], names[3],names[4],names[5],names[6],names[7],names[8],names[9]);/*read input strings from "input.txt"*/
  
   /*lets check your parser*/
   for(i=0;i<inputs;i++)
     {j=parse(names[i]);
-      switch(j)
-	{
-	case(0):fprintf(fpout,"%s is not a formula\n", names[i]);break;
-	case(1):fprintf(fpout,"%s is a proposition\n",names[i]);break;
-	case(2):fprintf(fpout,"%s is a negation\n",names[i]);break;
-	case(3):fprintf(fpout,"%s is a binary formula, the first part is %s and the second part is %s\n",names[i]), partone(names[i]), parttwo(names[i]);break;
-	default:fprintf(fpout,"%s is not a formula\n",names[i]);break;
-	}
+  //     switch(j)
+	// {
+	// case(0):fprintf(fpout,"%s is not a formula\n", names[i]);break;
+	// case(1):fprintf(fpout,"%s is a proposition\n",names[i]);break;
+	// case(2):fprintf(fpout,"%s is a negation\n",names[i]);break;
+	// case(3):fprintf(fpout,"%s is a binary formula, the first part is %s and the second part is %s\n",names[i]), partone(names[i]), parttwo(names[i]);break;
+	// default:fprintf(fpout,"%s is not a formula\n",names[i]);break;
+	// }
     }
  
  
@@ -59,5 +102,3 @@ int main()
  
   return(0);
 }
-1
-Downloading1
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+switch(j)
+	// {
+	// case(0):fprintf(fpout,"%s is not a formula\n", names[i]);break;
+	// case(1):fprintf(fpout,"%s is a proposition\n",names[i]);break;
+	// case(2):fprintf(fpout,"%s is a negation\n",names[i]);break;
+	// case(3):fprintf(fpout,"%s is a binary formula, the first part is %s and the second part is %s\n",names[i]), partone(names[i]), parttwo(names[i]);break;
+	// default:fprintf(fpout,"%s is not a formula\n",names[i]);break;
+	// }
